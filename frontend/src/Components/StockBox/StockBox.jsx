a88dce17c6fdffb16e9b3f6c53954311bc02ef6e
John Henry <johnhenryrudden@gmail.com>
2021-08-01 23:28:26 -0500

    checkboxes added

--- frontend/src/Components/StockBox/StockBox.jsx
+++ frontend/src/Components/StockBox/StockBox.jsx
@@ -1,18 +1,84 @@
 import React, { useState } from "react";
 import StockItem from "./StockItem/StockItem";
 import "./StockBox.css";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
 
 const StockBox = ({ stocks }) => {
   const [numStocks, setNumStocks] = useState(16);
+  const [sizeFilters, setSizeFilters] = useState({
+    micro: false,
+    small: false,
+    mid: false,
+    large: false,
+    mega: false,
+  });
+
+  const handleSizeChange = (e) => {
+    console.log(sizeFilters);
+    setSizeFilters({
+      ...sizeFilters,
+      [e.target.value]: !sizeFilters[e.target.value],
+    });
+  };
 
   return (
     <div className="stockBoxPage">
       <div className="filterButtons">
-        <button className="microButton">Micro</button>
-        <button className="smallButton">Small</button>
-        <button className="midButton">Mid</button>
-        <button className="largeButton">Large</button>
-        <button className="megaButton">Mega</button>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={sizeFilters.micro}
+              onChange={handleSizeChange}
+              value="micro"
+              aria-label="micro"
+            />
+          }
+          label="micro"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={sizeFilters.small}
+              onChange={handleSizeChange}
+              value="small"
+              aria-label="small"
+            />
+          }
+          label="small"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={sizeFilters.mid}
+              onChange={handleSizeChange}
+              value="mid"
+              aria-label="mid"
+            />
+          }
+          label="mid"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={sizeFilters.large}
+              onChange={handleSizeChange}
+              value="large"
+              aria-label="large"
+            />
+          }
+          label="large"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={sizeFilters.mega}
+              onChange={handleSizeChange}
+              value="mega"
+              aria-label="mega"
+            />
+          }
+          label="mega"
+        />
       </div>
       <div className="stockBox">
         {stocks
