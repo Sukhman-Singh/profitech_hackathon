0e217be292492f402199b392b27c301861956861
saketh.saripalli <saketh.saripalli@gmail.com>
2021-08-02 01:29:20 -0400

    Fixing stocks route

--- app.py
+++ app.py
@@ -10,11 +10,22 @@ def get_stock_list():
     if request.method == 'GET':
         income = request.args.get('income', default=100000, type=int)
         investment_amount = request.args.get('investment_amount', default=50000, type=int)
-        num_stocks = 100
+        num_stocks = 64
 
         stock_list = fv.get_stock_list_from_memory(income=income, investment_amount=investment_amount, num_stocks=num_stocks)
         return json.dumps({'stocks': stock_list})
-    #return json.dumps([])
+    return json.dumps({})
+
+@app.route('/stocks_real', methods=['GET','POST'])
+def get_stock_list_real():
+    if request.method == 'GET':
+        income = request.args.get('income', default=100000, type=int)
+        investment_amount = request.args.get('investment_amount', default=50000, type=int)
+        num_stocks = 64
+
+        stock_list = fv.get_stock_list(income=income, investment_amount=investment_amount, num_stocks=num_stocks)
+        return json.dumps({'stocks': stock_list})
+    return json.dumps({})
         
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
